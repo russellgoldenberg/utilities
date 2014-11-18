9eb7fc9d217b31c0304c1c9272591b5d426fa5d6
russell goldenberg <russellgoldenberg@gmail.com>
2014-11-18 10:57:40 -0500

    optimal image size

---
+++ utils/js/image-size.js
@@ -0,0 +1,21 @@
+function getOptimalImageSize(sizes) {
+	var trueScreenWidth = window.screen.width * window.devicePixelRatio;
+	var numSizes = sizes.length - 1;
+	var best = sizes[numSizes];
+
+	for(var i = numSizes; i > -1; i--) {
+		console.log(trueScreenWidth, sizes[i]);
+		if(trueScreenWidth > sizes[i]) {
+			break;
+		} else {
+			best = sizes[i];
+		}
+	}
+
+	return best;
+}
+
+// a list of image sizes (in width) available in ascending order
+var sizeList = [640, 960, 1280, 1920];
+
+var imgSize = getOptimalImageSize(sizeList);
