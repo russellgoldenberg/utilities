99e9e0f305f349a74294427bfafc74c0c2cf1465
russell goldenberg <russellgoldenberg@gmail.com>
2014-12-08 10:43:15 -0500

    deep copy array

---
+++ utils/js/copy-array.js
@@ -0,0 +1,8 @@
+function copyArray(input) {
+    var output = [];
+    for(var i = 0; i < input.length; i++) {
+        var clone = JSON.parse(JSON.stringify(input[i]));
+        output.push(clone);
+    }
+    return output;
+}
