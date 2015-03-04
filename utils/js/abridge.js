dff1e46ff5ef0ba7b2b69f6b963ccd496cb3fc5b
russell goldenberg <russellgoldenberg@gmail.com>
2015-03-03 21:23:57 -0500

    tweaks to abridge

--- utils/js/abridge.js
+++ utils/js/abridge.js
@@ -1,9 +1,10 @@
 // abridge({input: 'a string to truncate', charLength: 10, cleanBreak: true, ellipsis: true});
 var abridge = function(params) {
-	var sub = params.input.substring(0, Math.min(params.input.length, params.charLength));
 	var end = params.ellipsis ? '...' : '';
-	if(params.cleanBreak && sub.lastIndexOf(' ') > -1) {
-		return sub.substring(0, sub.lastIndexOf(' ')) + end;
+	var sub = params.input.substring(0, params.charLength);
+	var spaceIndex = sub.lastIndexOf(' ');
+	if(params.cleanBreak && spaceIndex > -1) {
+		return sub.substring(0, spaceIndex) + end;
 	} else {
 		return sub + end;
 	}
