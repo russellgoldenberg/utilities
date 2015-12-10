7686db5e703a46005aad67db2343a83ad62fccb6
russellgoldenberg <russellgoldenberg@gmail.com>
2015-12-10 17:57:02 -0500

    offset

--- utils/js/scroll-into-view.js
+++ utils/js/scroll-into-view.js
@@ -1,13 +1,26 @@
 var scrollIntoView = function(params) {
-
+	var distanceFromTop;
 	var elements = [];
-	var query = document.getElementsByClassName(params.selector);
-	for (var i = 0; i < query.length; i++) {
-		elements.push(query[i]);
-	}
 
-	// height of window
-	var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	var init = function() {
+		distanceFromTop = getOffset();
+		console.log(distanceFromTop);
+		var query = document.getElementsByClassName(params.selector);
+		for (var i = 0; i < query.length; i++) {
+			elements.push(query[i]);
+		}
+		window.addEventListener('scroll', onScroll, true);
+	};
+
+	var getOffset = function() {
+		if (params.offset) {
+			var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+			var num = parseInt(params.offset.replace('%', '')) / 100;
+			var fraction = Math.max(0, Math.min(100, num));
+			return h - fraction * h;
+		}
+		return 0;
+	};
 
 	var onScroll = function() {
 		var remove = [];
@@ -19,7 +32,7 @@ var scrollIntoView = function(params) {
 			var top = rect.top;
 			var bottom = rect.bottom;
 
-			if (top < height) {
+			if (top < distanceFromTop) {
 				params.func(el);
 				remove.push(i);
 			}
@@ -37,9 +50,9 @@ var scrollIntoView = function(params) {
 		}
 
 		if (!elements.length) {
-			window.removeEventListener('scroll', onScroll);
+			window.removeEventListener('scroll', onScroll, true);
 		}
-	}
+	};
 
-	window.addEventListener('scroll', onScroll);
+	init();
 };
